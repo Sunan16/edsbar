fd7d7db3292077a479731b16f165ad5e1dabd044
Sunan16 <pedrobraoliveira@gmail.com>
2024-04-05 11:19:35 -0300

    Value Object and remade the 'Model'

---
+++ src/main/java/SystemStoreDesk.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemStoreDesk {
+    
+    public static void main(String[] args) {
+		SpringApplication.run(System.class, args);
+	}
+
+}
